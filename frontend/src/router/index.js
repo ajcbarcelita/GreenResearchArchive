53a1938e19a166675c6a7f635ef77ed7752b0d07
Aaron Barcelita <aaron_barcelita@dlsu.edu.ph>
2026-02-07 19:38:40 +0800

    Initialized frontend project directory.

---
+++ frontend/src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // list them all here
+  ],
+})
+
+export default router
