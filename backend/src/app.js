0c752a68010acd3f765cb7cc6bf0454becf4a091
ajcbarcelita <aaron_barcelita@dlsu.edu.ph>
2026-02-16 14:04:47 +0800

    Added more packages for backend.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 
 dotenv.config();
 const app = express();
 
 // Set middleware
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Define routes here
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,18 @@
 import app from './app.js';
+import pino from 'pino';
 
 const PORT = process.env.PORT || 3000;
+const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      levelFirst: true,
+      translateTime: true
+    }
+  }
+})
 
 app.listen(PORT, () => {
-  console.log(`Backend is running on port ${PORT}`);
+  logger.info(`Backend is running on port ${PORT}`);
 });
